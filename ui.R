510c7954170806fac5c20b4cdab372ea40dd5acd
danithered <danithered@live.com>
2022-11-08 12:22:17 +0100

    tabsets
    	modified:   ui.R

--- ui.R
+++ ui.R
@@ -1,27 +1,65 @@
 library(shiny)
 
-shinyUI(fluidPage(
-    #tags$style(height="100vh", width="100vw"),
 
-    # Application title
-    h3("Report updater interface"),
-    
-    # Settings
-    #position= "fixed-top",
-div(
-    # Menu
-    div(fluidRow(
-            column(4,
-              textOutput("updatetime"),
-            ),
-            column(4,
-              actionButton("knit", "Update"),
-              actionButton("git", "Version update")
-            )
-        
-        , style="position:static; width:inherit;background-color:lightblue")
-    ),
-    DT::dataTableOutput("parameters"),
-    tableOutput("par")
-, style="height:90vh")
-))
+ui <- fluidPage(
+  
+  # App title ----
+  titlePanel("Report updater interface"),
+  
+  
+  
+  # Output: Tabset w/ plot, summary, and table ----
+  tabsetPanel(type = "tabs", 
+              tabPanel("Status", fluid=T,{
+                fluidPage(
+                  # Menu
+                  br(),
+                  fluidRow(
+                    column(4,
+                           textOutput("updatetime"),
+                    ),
+                    column(4,
+                           actionButton("knit", "Update"),
+                           actionButton("git", "Version update")
+                    )
+                    
+                    , style="position:static; width:inherit;background-color:lightblue"
+                  ),
+                  DT::dataTableOutput("parameters"),
+                  tableOutput("par")
+                )
+              }),
+              tabPanel("Summary", {
+                fluidPage(
+                  h3("bxv")
+                )
+              })
+  )
+)
+
+# shinyUI(fluidPage(
+#     #tags$style(height="100vh", width="100vw"),
+# 
+#     # Application title
+#     h3("Report updater interface"),
+#     
+#     # Settings
+#     #position= "fixed-top",
+# 
+# div(
+#     # Menu
+#     div(fluidRow(
+#             column(4,
+#               textOutput("updatetime"),
+#             ),
+#             column(4,
+#               actionButton("knit", "Update"),
+#               actionButton("git", "Version update")
+#             )
+#         
+#         , style="position:static; width:inherit;background-color:lightblue")
+#     ),
+#     DT::dataTableOutput("parameters"),
+#     tableOutput("par")
+# , style="height:90vh")
+# ))
